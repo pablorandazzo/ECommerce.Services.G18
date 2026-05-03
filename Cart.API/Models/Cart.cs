f0cfb6d48c9bc2df9594d78829205e539059d88f
Fran0012 <francodiiasi12@gmail.com>
2026-05-03 20:38:13 -0300

    Modelado de Cart y CartItem

--- Cart.API/Models/Cart.cs
+++ Cart.API/Models/Cart.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
-public class Class1
+namespace Cart.API.Models
 {
-	public Class1()
-	{
-	}
+    public class Cart
+    {
+        public Guid UsuarioId { get; set; } // Identificador del usuario dueño
+
+        /// <summary>
+        /// Lista de productos en el carrito.
+        /// La inicializamos como una lista vacía para que nunca sea null.
+        /// </summary>
+        public List<CartItem> Items { get; set; } = new List<CartItem>(); //
+
+        public DateTime FechaActualizacion { get; set; } // Se actualiza en cada operación
+    }
 }
