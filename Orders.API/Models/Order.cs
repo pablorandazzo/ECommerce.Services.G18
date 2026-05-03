c6c6ad6eb563a8fb340e4e4c429fcdbab8745dcc
Pablo <pablorandazzo2000@gmail.com>
2026-05-03 15:08:18 -0300

    Agrego los Models Order, OrderItem y Product

---
+++ Orders.API/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace Orders.API.Models;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Guid UsuarioId { get; set; }
+
+    // Inicializamos la lista para que nunca sea nula
+    public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+    public decimal Total { get; set; }
+    public string Estado { get; set; } = "Pendiente"; // Valor inicial por defecto
+    public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+}
---
+++ Orders.API/Models/OrderItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Orders.API.Models;
+
+public class OrderItem
+{
+    public Guid ProductoId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal PrecioUnitario { get; set; }
+}
