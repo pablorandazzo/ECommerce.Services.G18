126fd8f49f3d38584bb59bf1a4fdafe48c0062be
Pablo <pablorandazzo2000@gmail.com>
2026-05-07 19:00:05 -0300

    Agrego excepciones en Orders y Products

---
+++ Orders.API/Exceptions/BusinessRuleException.cs
@@ -0,0 +1,11 @@
+namespace Orders.API.Exceptions;
+
+public class BusinessRuleException : Exception
+{
+    public string ErrorCode { get; }
+
+    public BusinessRuleException(string errorCode, string message) : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+}
